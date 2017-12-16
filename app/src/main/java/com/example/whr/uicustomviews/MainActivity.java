79c4f0d965da66f6a091a635efe4487c4107f993
wanghaorangithub <240789872@qq.com>
2017-12-16 11:55:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/whr/uicustomviews/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.whr.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+    }
+
+}
